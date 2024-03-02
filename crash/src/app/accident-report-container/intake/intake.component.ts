2803a9f3b1b49bfd412064cc425e0491739569f0
Gonzales <mario.gonzales@cgi.com>
2024-03-02 13:18:37 -0700

    interface for accident report page

---
+++ crash/src/app/accident-report-container/intake/intake.component.ts
@@ -0,0 +1,99 @@
+
+import { Component ,OnInit  } from '@angular/core';
+import { CommonModule, DatePipe, JsonPipe } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormBuilder,FormGroup,FormControl, ReactiveFormsModule ,Validators} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {NgxMatTimepickerModule} from 'ngx-mat-timepicker';  
+import { MapComponent} from '../map/map.component'
+import { UploadComponent } from '../upload-images/upload.component'; 
+
+@Component({
+  selector: 'crash-intake',
+  standalone: true,
+  providers: [provideNativeDateAdapter(), DatePipe],
+  imports: [CommonModule, RouterOutlet,  ReactiveFormsModule,
+    MatFormFieldModule,MatDatepickerModule,
+    NgxMatTimepickerModule , MapComponent, UploadComponent
+  ],
+  templateUrl: './intake.component.html',
+  styleUrl: './intake.component.scss'
+})
+
+
+export class IntakeComponent  implements OnInit  {
+
+  form : FormGroup = new FormGroup({
+    DateIncident: new FormControl(new Date(),[Validators.required]),
+    TimeIncident: new FormControl('',[Validators.required]),
+    Party : new FormControl('',[Validators.required]),
+    Location  : new FormControl('',[Validators.required]),
+    WeatherConditions: new FormControl(),
+    EstimateCost: new FormControl('',[Validators.required]),
+    NumPartiesInvolved:new FormControl(),
+      
+});
+WeatherIcon:string =''
+Image1: any;
+Image2: any;
+Image3: any;
+ 
+  selectedFile: File | null = null;
+
+  constructor(private formBuilder: FormBuilder ) {
+ 
+  }
+   
+  public location: string='';
+  public weatherconditions: string='';
+  submitted = false;
+  ngOnInit() {
+    var datePipe = new DatePipe('en-US');
+    this.form.controls["TimeIncident"].setValue(datePipe.transform(new Date(),'h:mm a'));
+  }
+ 
+ 
+getAddress(thelocation : any){
+    this.form.controls["Location"].setValue(thelocation);
+   
+}
+ 
+getWeather(theweather:any){
+    this.form.controls["WeatherConditions"].setValue(theweather);
+   }
+getWeatherIcon(theweathericon:any){
+      this.WeatherIcon=theweathericon;
+      console.log(theweathericon)
+   }
+getImage1(img:any){
+  this.Image1=img;
+}
+getImage2(img:any){
+  this.Image2=img;
+}
+getImage3(img:any){
+  this.Image3=img;
+}
+
+
+
+onSubmit(): void{
+  this.submitted = true;
+  if (this.form.invalid) {
+    console.log("Field validations failed")
+    return;
+  }
+  console.log(
+    `You entered :  
+    ${this.form.controls['Party'].value},
+    ${this.form.controls['WeatherConditions'].value},
+    ${this.form.controls['Location'].value}`
+  );
+  console.log(this.Image1)
+  console.log(this.Image2)
+  console.log(this.Image3)
+}
+
+}
