e3c6ba52442979ebb1efcb8202eb5c32e82d22d0
Ian Baguio <ianrey_2004@yahoo.com>
2024-03-17 17:41:55 -0600

    chore: setup api and postgres db

---
+++ api/crash/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crash.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crash.Database
+{
+   public class AppDbContext: DbContext
+   {
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+      {
+      }
+
+      public DbSet<Accident> accident { get; set;}
+      public DbSet<CrashUser> crashuser { get; set;}
+   }
+}
