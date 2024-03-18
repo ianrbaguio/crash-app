aa06f6a4a49d764ee2d223448f1d805e1fc36e28
Ian Baguio <ianrey_2004@yahoo.com>
2024-03-18 10:26:17 -0600

    chore: add note to AppDbContext

--- api/crash/Database/AppDbContext.cs
+++ api/crash/Database/AppDbContext.cs
@@ -9,6 +9,12 @@ namespace Crash.Database
       {
       }
 
+      /*
+         NOTE: When adding new table in the future, we'll have to name it in lower case
+               as for some reason when doing the migrations using dotnet ef, it creates
+               the table name case-sensitive. Using CREATE TABLE [name] in postgres or pgAdmin
+               it creates the table names in all lower-case.
+      */
       public DbSet<Accident> accident { get; set;}
       public DbSet<CrashUser> crashuser { get; set;}
    }
